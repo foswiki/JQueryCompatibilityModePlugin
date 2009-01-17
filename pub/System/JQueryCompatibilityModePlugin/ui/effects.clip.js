8cb4ed51b20e70a192c97049557a92de7734ecdf
EugenMayer <EugenMayer@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-01-17 00:14:34 +0000

    Item755: updated Jquery and Jquery-UI on 1.3
    git-svn-id: http://svn.foswiki.org/trunk/JQueryCompatibilityModePlugin@2027 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- pub/System/JQueryCompatibilityModePlugin/ui/effects.clip.js
+++ pub/System/JQueryCompatibilityModePlugin/ui/effects.clip.js
@@ -1,23 +1,23 @@
 /*
- * jQuery UI Effects Blind 1.6rc5
+ * jQuery UI Effects Clip 1.6rc5
  *
  * Copyright (c) 2009 AUTHORS.txt (http://ui.jquery.com/about)
  * Dual licensed under the MIT (MIT-LICENSE.txt)
  * and GPL (GPL-LICENSE.txt) licenses.
  *
- * http://docs.jquery.com/UI/Effects/Blind
+ * http://docs.jquery.com/UI/Effects/Clip
  *
  * Depends:
  *	effects.core.js
  */
 (function($) {
 
-$.effects.blind = function(o) {
+$.effects.clip = function(o) {
 
 	return this.queue(function() {
 
 		// Create element
-		var el = $(this), props = ['position','top','left'];
+		var el = $(this), props = ['position','top','left','height','width'];
 
 		// Set options
 		var mode = $.effects.setMode(el, o.options.mode || 'hide'); // Set Mode
@@ -26,21 +26,26 @@ $.effects.blind = function(o) {
 		// Adjust
 		$.effects.save(el, props); el.show(); // Save & Show
 		var wrapper = $.effects.createWrapper(el).css({overflow:'hidden'}); // Create Wrapper
-		var ref = (direction == 'vertical') ? 'height' : 'width';
-		var distance = (direction == 'vertical') ? wrapper.height() : wrapper.width();
-		if(mode == 'show') wrapper.css(ref, 0); // Shift
+		var animate = el[0].tagName == 'IMG' ? wrapper : el;
+		var ref = {
+			size: (direction == 'vertical') ? 'height' : 'width',
+			position: (direction == 'vertical') ? 'top' : 'left'
+		};
+		var distance = (direction == 'vertical') ? animate.height() : animate.width();
+		if(mode == 'show') { animate.css(ref.size, 0); animate.css(ref.position, distance / 2); } // Shift
 
 		// Animation
 		var animation = {};
-		animation[ref] = mode == 'show' ? distance : 0;
+		animation[ref.size] = mode == 'show' ? distance : 0;
+		animation[ref.position] = mode == 'show' ? 0 : distance / 2;
 
 		// Animate
-		wrapper.animate(animation, o.duration, o.options.easing, function() {
+		animate.animate(animation, { queue: false, duration: o.duration, easing: o.options.easing, complete: function() {
 			if(mode == 'hide') el.hide(); // Hide
 			$.effects.restore(el, props); $.effects.removeWrapper(el); // Restore
 			if(o.callback) o.callback.apply(el[0], arguments); // Callback
 			el.dequeue();
-		});
+		}});
 
 	});
 
